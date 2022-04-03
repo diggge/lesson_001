e1fc6343564328c7436215cf557ffc2660ecfa64
anatoly <diggge@mail.ru>
2022-04-03 20:41:17 +0900

    Initial commit

---
+++ red_hat/os_info.py
@@ -0,0 +1,10 @@
+#TODO запустить этот скрипт а закомитить результат его работы
+
+import ...
+
+info = 'OS info is \n {} \n\n Python version is {} {}'.format (
+    platform.uname(), sys.version, platform.architecture())
+print(info)
+
+with open ('os_info.txt', 'w') as ff:
+    ff.write(info)
